0f51ddeb75df193ad5997495313046aa1ccacc66
Stefan Aleksic <stefan@stefanaleksic.com>
2015-12-19 22:40:22 -0500

    Add security constants

--- main.go
+++ main.go
@@ -1,3 +1,26 @@
-package rfbServer
+package main
 
+
+import (
+)
+
+const (
+	INVALID = 0
+	NONE = 1
+	VNC_AUTH = 2
+	RA2 = 5
+	RA2ne = 6
+	Tight = 16
+	Ultra = 17
+	TLS = 18
+	VENCRYPT = 19
+	GTK_VNC_SASL = 20
+	MD5_HASH = 21
+	COLIN_DEAN_XVP = 22
+)
+
+func main() {
+
+
+}
 
