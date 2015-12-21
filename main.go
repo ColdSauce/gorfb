2bb552044e58bde9ad48be9a85bb1cbfbbd1e288
Stefan Aleksic <stefan@stefanaleksic.com>
2015-12-20 23:06:19 -0600

    Create security handle function

--- main.go
+++ main.go
@@ -4,20 +4,38 @@ package main
 import (
 )
 
-const (
-	INVALID = 0
-	NONE = 1
-	VNC_AUTH = 2
-	RA2 = 5
-	RA2ne = 6
-	Tight = 16
-	Ultra = 17
-	TLS = 18
-	VENCRYPT = 19
-	GTK_VNC_SASL = 20
-	MD5_HASH = 21
-	COLIN_DEAN_XVP = 22
-)
+
+
+func doSecurityHandshake() {
+
+	const (
+		INVALID = 0
+		NONE = 1
+		VNC_AUTH = 2
+		RA2 = 5
+		RA2ne = 6
+		Tight = 16
+		Ultra = 17
+		TLS = 18
+		VENCRYPT = 19
+		GTK_VNC_SASL = 20
+		MD5_HASH = 21
+		COLIN_DEAN_XVP = 22
+	)
+
+	type securityResult struct {
+		message uint32 // can be either 0 or 1.
+			       // 0 = OK
+			       // 1 = Failed
+	}
+
+	type reasonForFailure struct {
+		reasonLength uint32
+		reasonString []byte
+	}
+
+
+}
 
 func main() {
 
